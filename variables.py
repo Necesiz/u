5ebd2881f8eb0d7f2a1e9ee362af35a2e749b57c
Teamabasof <104113926+Teamabasof@users.noreply.github.com>
2022-12-27 03:48:32 +0400

    Update variables.py

--- variables.py
+++ variables.py
@@ -5,3 +5,7 @@ from os import environ
 id_pattern = re.compile(r'^.\d+$')
 
 ADMIN = os.environ.get("ADMIN", "5134595693")
+
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://music:music@cluster0.sh6h4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+
+DB_NAME = os.environ.get("DB_NAME", "music")
