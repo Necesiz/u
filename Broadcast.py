130114814c8aa4e2eecc56bef17185c4fa737644
Teamabasof <104113926+Teamabasof@users.noreply.github.com>
2022-12-27 06:49:00 +0400

    Update Broadcast.py

--- Broadcast.py
+++ Broadcast.py
@@ -22,13 +22,13 @@ app = Client(
     bot_token=variables.BOT_TOKEN,
 )
  
-@Client.on_message(filters.command("users") & filters.user(OWNER_ID))
+@client.on_message(filters.command("users") & filters.user(OWNER_ID))
 async def get_stats(bot :Client, message: Message):
     mr = await message.reply('**ACCESSING DETAILS.....**')
     total_users = await db.total_users_count()
     await mr.edit( text=f"🔍 TOTAL USER'S = `{total_users}`")
 
-@Client.on_message(filters.command("yolla") & filters.user(OWNER_ID) & filters.reply)
+@client.on_message(filters.command("yolla") & filters.user(OWNER_ID) & filters.reply)
 async def broadcast_handler(bot: Client, m: Message):
     all_users = await db.get_all_users()
     broadcast_msg = m.reply_to_message
