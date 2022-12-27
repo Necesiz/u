741a2f7c3257df572c25b2259752a94787dbb67f
Teamabasof <104113926+Teamabasof@users.noreply.github.com>
2022-12-27 05:58:34 +0400

    Update Broadcast.py

--- Broadcast.py
+++ Broadcast.py
@@ -13,20 +13,20 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 
 
-app = Client(
+app = client(
     "OLD-TAGGER-BOT",
     "api_id=variables.API_ID",
     "api_hash=variables.API_HASH",
     "bot_token=variables.BOT_TOKEN",
 )
  
-@client.on_message(filters.command("users") & filters.user(OWNER_ID))
+@client.on_message(filters.command("users") & filters.user(ADMIN))
 async def get_stats(bot :Client, message: Message):
     mr = await message.reply('**ACCESSING DETAILS.....**')
     total_users = await db.total_users_count()
     await mr.edit( text=f"🔍 TOTAL USER'S = `{total_users}`")
 
-@client.on_message(filters.command("yolla") & filters.user(OWNER_ID) & filters.reply)
+@client.on_message(filters.command("yolla") & filters.user(ADMIN) & filters.reply)
 async def broadcast_handler(bot: Client, m: Message):
     all_users = await db.get_all_users()
     broadcast_msg = m.reply_to_message
