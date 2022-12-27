4c98efa3887b44b4eeb80faaae66cc3238c58b0a
Teamabasof <104113926+Teamabasof@users.noreply.github.com>
2022-12-27 04:29:41 +0400

    Update variables.py

--- variables.py
+++ variables.py
@@ -6,10 +6,9 @@ id_pattern = re.compile(r'^.\d+$')
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "5740066159:AAF358Qc2PKTQsFJoqcITBobgVyrIxJbV6c")
 
-API_ID = int(os.environ.get("API_ID", "15954332"))
+API_ID = int(os.environ.get("API_ID","15954332"))
 
-API_HASH = os.environ.get("API_HASH", "85adea6f1eaf068b707703b4846a9ced")
-
+API_HASH = os.environ.get("API_HASH","85adea6f1eaf068b707703b4846a9ced")
 ADMIN = os.environ.get("ADMIN", "5134595693")
 
 DB_URL = os.environ.get("DB_URL", "mongodb+srv://music:music@cluster0.sh6h4.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
