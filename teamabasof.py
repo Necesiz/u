de3596cfe5c2d310cf1c78ccfb335c2a28624d9f
Teamabasof <104113926+Teamabasof@users.noreply.github.com>
2022-12-28 09:54:57 +0400

    update teamabasof.py

--- teamabasof.py
+++ teamabasof.py
@@ -46,6 +46,18 @@ async def id(bot, update):
     )
     
     
+@app.on_message(filters.command("restart") & filters.user(OWNER_ID))
+async def restart_bot(_, message: Message):
+    try:
+        msg = await message.reply_text("`Bot yeniden basladilir...`")
+        LOGGER(__name__).info("BOT serverde yeniden basladildi !!")
+    except BaseException as err:
+        LOGGER(__name__).info(f"{err}")
+        return
+    await msg.edit_text("✅ Bot has restarted !\n\n")
+    os.system(f"kill -9 {os.getpid()} && bash start")
+
+    
 @app.on_message(filters.command("info"))
 async def info(bot, update):
     
