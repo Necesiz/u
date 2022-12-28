91e521d32670c2ed70c8d5e9bb19382c38a2d0c2
Teamabasof <104113926+Teamabasof@users.noreply.github.com>
2022-12-28 04:36:23 +0400

    Update teamabasof.py

--- teamabasof.py
+++ teamabasof.py
@@ -31,38 +31,6 @@ async def hg(bot: Client, msg: Message):
             await msg.reply('İşte bu gelen benim sahibim.')
 
 
-@app.on_message(filters.command("purge", COMMAND_HAND_LER) & admin_fliter)
-async def purge(client, message):
-    """ purge upto the replied message """
-    if message.chat.type not in [ChatType.SUPERGROUP, ChatType.CHANNEL]:
-        # https://t.me/c/1312712379/84174
-        return
-
-    status_message = await message.reply_text("...", quote=True)
-    await message.delete()
-    message_ids = []
-    count_del_etion_s = 0
-
-    if message.reply_to_message:
-        for a_s_message_id in range(
-            message.reply_to_message.id, message.id
-        ):
-            message_ids.append(a_s_message_id)
-            if len(message_ids) == TG_MAX_SELECT_LEN:
-                count_del_etion_s += await client.delete_messages(
-                    chat_id=message.chat.id, message_ids=message_ids, revoke=True
-                )
-                message_ids = []
-        if len(message_ids) > 0:
-            count_del_etion_s += await client.delete_messages(
-                chat_id=message.chat.id, message_ids=message_ids, revoke=True
-            )
-
-    await status_message.edit_text(f"deleted {count_del_etion_s} messages")
-    await asyncio.sleep(5)
-    await status_message.delete()
-
-
 @app.on_message(filters.command("ping"))
 async def pingy(client, message):
     start = datetime.now()
