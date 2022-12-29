47bf30498800ddfea571cf2bc8235f8debd7835c
Teamabasof <104113926+Teamabasof@users.noreply.github.com>
2022-12-29 23:24:25 +0400

    update teamabasof.py

--- teamabasof.py
+++ teamabasof.py
@@ -11,7 +11,7 @@ import random, re
 import wget
 import os, youtube_dl, requests, time
 from Config import Config
-from youtube_search import YoutubeSearch
+import YoutubeSearch
 import lyricsgenius
 from pyrogram.handlers import MessageHandler
 from pyrogram import Client, filters
