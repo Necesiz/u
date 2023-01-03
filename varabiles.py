d6fe792a060be9fd8c42aeeb85047eec0351d0a7
Teamabasof <104113926+Teamabasof@users.noreply.github.com>
2023-01-04 01:34:53 +0400

    Update varabiles.py

--- varabiles.py
+++ varabiles.py
@@ -6,4 +6,4 @@ admins = {}
 UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Teamabasof/OLD-TAGGER-BOT")
 BOT_USERNAME = getenv("BOT_USERNAME", "OldMultiBot")
 SUDO_USERS = list(map(int, getenv("SUDO_USERS").split()))
-COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
+COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .")
