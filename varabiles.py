40b643915fb12ced4c989693f576500d86199b4d
Teamabasof <104113926+Teamabasof@users.noreply.github.com>
2023-01-04 01:32:42 +0400

    Update varabiles.py

--- varabiles.py
+++ varabiles.py
@@ -1,5 +1,7 @@
 import os
 from os import getenv
+
+class varabiles():
 admins = {}
 UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Teamabasof/OLD-TAGGER-BOT")
 BOT_USERNAME = getenv("BOT_USERNAME", "OldMultiBot")
