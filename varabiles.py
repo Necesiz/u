a56c77727768036fefdcb706f66b65bfeac8f8cb
Teamabasof <104113926+Teamabasof@users.noreply.github.com>
2023-01-04 01:31:04 +0400

    Update varabiles.py

--- varabiles.py
+++ varabiles.py
@@ -1,11 +1,5 @@
 import os
 from os import getenv
-from dotenv import load_dotenv
-
-if os.path.exists("local.env"):
-    load_dotenv("local.env")
-
-load_dotenv()
 admins = {}
 UPSTREAM_REPO = getenv("UPSTREAM_REPO", "https://github.com/Teamabasof/OLD-TAGGER-BOT")
 BOT_USERNAME = getenv("BOT_USERNAME", "OldMultiBot")
