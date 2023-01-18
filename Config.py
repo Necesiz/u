49e7d3ed22cd952de5395eeecdca4fe62f3489ed
Fakebody31 <99437747+Fakebody31@users.noreply.github.com>
2023-01-18 19:38:37 +0400

    Update Config.py

--- Config.py
+++ Config.py
@@ -7,7 +7,7 @@ class Config():
     admins = {}
     API_ID = int(os.environ.get("API_ID","15954332"))
     API_HASH = os.environ.get("API_HASH","85adea6f1eaf068b707703b4846a9ced")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5740066159:AAHfcRYqDQAIQjoCX4kkTXgdsP06AED2E14")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5740066159:AAFi9y9SVjZGDWzNxUfCKzdVW75Dhc4iMYw")
     BOT_USERNAME = os.environ.get("BOT_USERNAME", "oldtaggerBot")
     BOT_NAME = os.environ.get("BOT_NAME", "OLD TAGGER")
     BOT_ID = int(os.environ.get("BOT_ID", "5964973513"))
