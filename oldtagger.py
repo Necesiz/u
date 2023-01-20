115977e2fe9cf3831aebfe413273df6205565e10
Necesiz <97000998+Necesiz@users.noreply.github.com>
2023-01-21 01:54:42 +0400

    Update oldtagger.py

--- oldtagger.py
+++ oldtagger.py
@@ -27,6 +27,13 @@ from youtube_search import YoutubeSearch
 from pyrogram.handlers import MessageHandler
 from pyrogram import Client, filters
 import yt_dlp
+from pyrogram import filters
+from aiohttp import ClientSession
+from pyrogram import Client
+from helper.fsub import ForceSub
+from helper.functions import make_carbon
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
+aiohttpsession = ClientSession()
 from pyrogram.errors import (
     FloodWait,
     InputUserDeactivated,
@@ -1431,6 +1438,35 @@ def bul(_, message):
     except Exception as e:
         print(e)
 
+
+
+@app.on_message(filters.command("carbon"))
+async def carbon_func(_, message):
+    FSub = await ForceSub(_, message)
+    if FSub == 400:
+        return
+    if not message.reply_to_message:
+        return await message.reply_text(
+            "ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴛᴇxᴛ ᴍᴇssᴀɢᴇ ᴛᴏ ᴍᴀᴋᴇ ᴄᴀʀʙᴏɴ."
+        )
+    if not message.reply_to_message.text:
+        return await message.reply_text(
+            "ʀᴇᴘʟʏ ᴛᴏ ᴀ ᴛᴇxᴛ ᴍᴇssᴀɢᴇ ᴛᴏ ᴍᴀᴋᴇ ᴄᴀʀʙᴏɴ."
+        )
+    user_id = message.from_user.id
+    m = await message.reply_text("ᴘʀᴏᴄᴇssɪɴɢ...")
+    carbon = await make_carbon(message.reply_to_message.text)
+    await m.edit("ᴜᴘʟᴏᴀᴅɪɴɢ..")
+    await message.reply_photo(
+        photo=carbon,
+        caption="**MADE WITH ❤️ BY >JEOL**",
+        reply_markup=InlineKeyboardMarkup( [[
+            InlineKeyboardButton("JOIN CHANNEL", url="https://t.me/beta_boTZ")                  
+            ]]
+        )
+    )
+    await m.delete()
+    carbon.close()
 	
 	
      
