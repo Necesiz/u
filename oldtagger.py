967c62652da982bf4fe0e85dca7fdafdc5364ada
Necesiz <97000998+Necesiz@users.noreply.github.com>
2023-01-21 18:23:57 +0400

    Update oldtagger.py

--- oldtagger.py
+++ oldtagger.py
@@ -10,6 +10,8 @@ from random import randint
 import configparser
 from asyncio import sleep
 from telethon import events
+from html_telegraph_poster.upload_images import upload_image
+from os import remove
 from Config import Config 
 # Pyrogram----------------------------------------------------------------------------------------------------
 import datetime
@@ -1511,6 +1513,21 @@ async def quiz(event):
 async def handler(event): # Welcome every new user 
     if event.user_joined: 
        await event.reply('Salam xos geldiniz groupa!')
+
+
+@client.register(events.NewMessage(outgoing=True, pattern=r'\.tgu'))
+async def runtgu(event):
+    await event.edit("Uploading...")
+    getcontent = await event.get_reply_message()
+    messagelocation = event.to_id
+    try:
+        targetcontent = await getcontent.download_media()
+        uploadcontent = upload_image(targetcontent)
+    except:
+        return await event.edit("Something Went Wrong")
+    await event.delete()
+    await event.client.send_message(messagelocation, uploadcontent)
+    remove(targetcontent)
 
   
 #@client.on(events.NewMessage(pattern='/reklam'))
