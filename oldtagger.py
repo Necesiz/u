3d26fe1b10be2beab7132208fd17f033ed5199c0
Necesiz <97000998+Necesiz@users.noreply.github.com>
2023-01-21 14:19:39 +0400

    Update oldtagger.py

--- oldtagger.py
+++ oldtagger.py
@@ -4,6 +4,10 @@ from telethon import TelegramClient, events
 from telethon.sessions import StringSession
 from telethon.tl.types import ChannelParticipantsAdmins
 from telethon import events, Button
+from telethon.tl.custom import Button
+import random # pip install random
+from random import randint
+import configparser
 from asyncio import sleep
 from Config import Config 
 # Pyrogram----------------------------------------------------------------------------------------------------
@@ -1464,11 +1468,41 @@ async def roll_throw(bot, message):
 async def roll_bowling(bot, message):
     await bot.send_dice(message.chat.id, "🎳") 
 
+### Quiz command
+@client.on(events.NewMessage(pattern='/(?i)quiz')) 
+async def quiz(event):
+    # get the sender
+    sender = await event.get_sender()
+    SENDER = sender.id
 
-@app.on_message(filters.command("say"))
-def echo(bot, message):
-    text msg.text.split(None, 1) [1]
-    msg.reply(text)
+    # Start a conversation
+    async with client.conversation(await event.get_chat(), exclusive=True) as conv:
+        # get two random numbers between 1 and 10
+        rand1 = randint(1,10)
+        rand2 = randint(1,10)
+        # make the sum
+        sum = rand1+rand2
+        # make another sum based on two different random numbers. This will be used for the wrong option
+        sum_not_true = randint(1,10) + randint(1,10)
+
+        # To make the position of the button random, let's define two keyboard that activates with 50% probability
+        if(bool(random.getrandbits(1))):
+            keyboard = [[Button.inline("{}".format(sum), sum)], [Button.inline("{}".format(sum_not_true), sum_not_true)]]
+        else:
+            keyboard = [[Button.inline("{}".format(sum_not_true), sum_not_true)],[Button.inline("{}".format(sum), sum)]]
+
+        text = "<b>Quiz time</b> 🤖\n{} + {} = ?\n".format(str(rand1), str(rand2))
+        await conv.send_message(text, buttons=keyboard, parse_mode='html')
+        press = await conv.wait_event(press_event(SENDER))
+        choice = str(press.data.decode("utf-8"))
+
+        if(choice == str(sum)):
+            await conv.send_message("Correct Answer!", parse_mode='html')
+        else:
+            await conv.send_message("Nope, i won!", parse_mode='html')
+
+        await conv.cancel_all()
+        return 
  
 
 
