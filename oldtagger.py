9ee1b8bac81a87d1bd7b34bee19dfeed3da09760
Necesiz <97000998+Necesiz@users.noreply.github.com>
2023-01-22 17:40:12 +0400

    Update oldtagger.py

--- oldtagger.py
+++ oldtagger.py
@@ -42,6 +42,7 @@ import pyrogram
 import os
 import asyncio
 from pyrogram import Client, filters
+import speedtest
 from pyrogram.errors import (
     FloodWait,
     InputUserDeactivated,
@@ -1546,7 +1547,45 @@ async def live(client: Client, message: Message):
     livemsg = await message.reply_text(ALIVE)
 
 
-#
+def testspeed(m):
+    try:
+        test = speedtest.Speedtest()
+        test.get_best_server()
+        m = m.edit("Running Download SpeedTest")
+        test.download()
+        m = m.edit("Running Upload SpeedTest")
+        test.upload()
+        test.results.share()
+        result = test.results.dict()
+        m = m.edit("Sharing SpeedTest Results")
+    except Exception as e:
+        return m.edit(e)
+    return result
+
+
+@app.on_message(filters.command("spedtest") & OWNER_ID)
+async def speedtest_function(client, message):
+    m = await message.reply_text("Running Speed test")
+    loop = asyncio.get_event_loop()
+    result = await loop.run_in_executor(None, testspeed, m)
+    output = f"""**Speedtest Results**
+    
+<u>**Client:**</u>
+**__ISP:__** {result['client']['isp']}
+**__Country:__** {result['client']['country']}
+  
+<u>**Server:**</u>
+**__Name:__** {result['server']['name']}
+**__Country:__** {result['server']['country']}, {result['server']['cc']}
+**__Sponsor:__** {result['server']['sponsor']}
+**__Latency:__** {result['server']['latency']}  
+**__Ping:__** {result['ping']}"""
+    msg = await app.send_photo(
+        chat_id=message.chat.id, 
+        photo=result["share"], 
+        caption=output
+    )
+    await m.delete()
 
   
 #@client.on(events.NewMessage(pattern='/reklam'))
