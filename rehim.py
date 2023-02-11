8423113f24f18e6fd25bbf55077e9619f89b7982
Necesiz <97000998+Necesiz@users.noreply.github.com>
2023-02-12 02:59:46 +0400

    Update rehim.py

--- rehim.py
+++ rehim.py
@@ -10,7 +10,7 @@ bot_token = Config.BOT_TOKEN
 #-#-#-# Pyrogram Başlanğıc #-#-#-#
 rehim = Client(":memory:", api_id, api_hash, bot_token=bot_token)
 
-@rehim.on_message(filters.private)
+@rehim.on_message(filters.command("start"))
 async def hello(client, message): 
     await message.reply("Hello from Pyrogram!")
 
@@ -38,6 +38,19 @@ def botlist(client, message):
     message.reply(response_text)
 
 
+adminList = ['teamabasov', 'anonyumaz', 'anonyumAzbot']
+
+def get_admin_list():
+  return adminList
+
+@rehim.on_message(filters.command(["adminlist"]))
+def adminlist(client, message):
+  text = f"The current admins are: {', '.join(get_admin_list())}"
+  client.send_message(
+    chat_id=message.chat.id, 
+    text=text, 
+    reply_to_message_id=message.message_id
+  )
 
 
 
