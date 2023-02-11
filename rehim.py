535063c8412d38fe9d57ad19417598073709cc36
Necesiz <97000998+Necesiz@users.noreply.github.com>
2023-02-12 02:48:23 +0400

    Update rehim.py

--- rehim.py
+++ rehim.py
@@ -10,29 +10,9 @@ bot_token = Config.BOT_TOKEN
 #-#-#-# Pyrogram Başlanğıc #-#-#-#
 rehim = Client(":memory:", api_id, api_hash, bot_token=bot_token)
 
-
-
-
-@rehim.on_message(filters.command("botlist"))
-def botlist(client, message):
-    bots = [     # List of Bots
-        {
-            "name": "OLDMULTİ",
-            "username": "oldMultiBot"
-        },
-        {
-            "name": "OLDTAGGER",
-            "username": "oldtaggerbot"
-        },
-        {
-            "name": "Cally",
-            "username": "CallyBot"
-        }
-    ]
-    response_text = ""
-    for bot in bots:
-        response_text += f"{bot['name']}: @{bot['username']}\n"
-    message.reply(response_text)
+@rehim.on_message(filters.private)
+async def hello(client, message): 
+    await message.reply("Hello from Pyrogram!")
 
 
 
