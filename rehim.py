c0894b9b5d409e34bee6d916617cd9316810c4a6
Necesiz <97000998+Necesiz@users.noreply.github.com>
2023-02-13 19:28:07 +0400

    Update rehim.py

--- rehim.py
+++ rehim.py
@@ -17,6 +17,30 @@ rehim = Client(":memory:", api_id, api_hash, bot_token=bot_token)
 
 
 
+
+
+@rehim.on_message(filters.command(["purge"])) 
+def purge(client, message): 
+    # Mesaj silmeyi kontrol edin
+    if message.chat.type == "supergroup":
+      messages = client.get_history( 
+              chat_id=message.chat.id, 
+              limit=1000
+        )
+
+      for msg in messages:
+        client.delete_messages(
+            chat_id=message.chat.id,
+            message_ids=msg.message_id
+        )
+
+      # Son mesaja cevap mesajı gönderin
+      client.send_message(
+        chat_id=message.chat.id,
+        text="Mesajlar başarıyla temizlendi!"
+      )
+
+
 @rehim.on_message(filters.command("sill"))
 def text_delete(client, message):
     # İlgili mesajı almak
