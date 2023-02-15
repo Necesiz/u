72e11580ec0012058eb7da2461f41cf9aeb065f4
Necesiz <97000998+Necesiz@users.noreply.github.com>
2023-02-15 22:33:51 +0400

    Update rehim.py

--- rehim.py
+++ rehim.py
@@ -24,116 +24,22 @@ downloads = './downloads/{}/'
 rehim = Client(":memory:", api_id, api_hash, bot_token=bot_token)
 
 
-DL_BUTTONS=[
-    [
-        InlineKeyboardButton("No Watermark", callback_data="nowm"),
-        InlineKeyboardButton("Watermark", callback_data="wm"),
-    ],
-    [InlineKeyboardButton("Audio", callback_data="audio")],
-]
-
-
-
-async def run_cmd(cmd: str) -> Tuple[str, str, int, int]:
-  args = shlex.split(cmd)
-  process = await asyncio.create_subprocess_exec(
-      *args, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
-  )
-  stdout, stderr = await process.communicate()
-  return (
-      stdout.decode("utf-8", "replace").strip(),
-      stderr.decode("utf-8", "replace").strip(),
-      process.returncode,
-      process.pid,
-  )
-
-
-
-# Downloader for tiktok
-@rehim.on_message(filters.regex(pattern='.*http.*') & filters.private)
-async def _tiktok(client, update):
-  url = update.text
-  session = requests.Session()
-  resp = session.head(url, allow_redirects=True)
-  if not 'tiktok.com' in resp.url:
-    return
-  await update.reply('Select the options below', True, reply_markup=InlineKeyboardMarkup(DL_BUTTONS))
-
-# Callbacks
-@rehim.on_callback_query()
-async def _callbacks(client, cb: CallbackQuery):
-  if cb.data == 'nowm':
-    dirs = downloads.format(uuid.uuid4().hex)
-    os.makedirs(dirs)
-    cbb = cb
-    update = cbb.message.reply_to_message
-    await cb.message.delete()
-    url = update.text
-    session = requests.Session()
-    resp = session.head(url, allow_redirects=True)
-    if '?' in resp.url:
-      tt = resp.url.split('?', 1)[0]
-    else:
-      tt = resp.url
-    ttid = dirs+tt.split('/')[-1]
-    r = requests.get('https://api.reiyuura.me/api/dl/tiktok?url='+tt)
-    result = r.text
-    rs = json.loads(result)
-    link = rs['result']['nowm']
-    resp = session.head(link, allow_redirects=True)
-    r = requests.get(resp.url, allow_redirects=True)
-    open(f'{ttid}.mp4', 'wb').write(r.content)
-    await client.send_video(update.chat.id, f'{ttid}.mp4',)
-    shutil.rmtree(dirs)
-  elif cb.data == 'wm':
-    dirs = downloads.format(uuid.uuid4().hex)
-    os.makedirs(dirs)
-    cbb = cb
-    update = cbb.message.reply_to_message
-    await cb.message.delete()
-    url = update.text
-    session = requests.Session()
-    resp = session.head(url, allow_redirects=True)
-    if '?' in resp.url:
-      tt = resp.url.split('?', 1)[0]
-    else:
-      tt = resp.url
-    ttid = dirs+tt.split('/')[-1]
-    r = requests.get('https://api.reiyuura.me/api/dl/tiktok?url='+tt)
-    result = r.text
-    rs = json.loads(result)
-    link = rs['result']['wm']
-    resp = session.head(link, allow_redirects=True)
-    r = requests.get(resp.url, allow_redirects=True)
-    open(f'{ttid}.mp4', 'wb').write(r.content)
-    await bot.send_video(update.chat.id, f'{ttid}.mp4',)
-    shutil.rmtree(dirs)
-  elif cb.data == 'audio':
-    dirs = downloads.format(uuid.uuid4().hex)
-    os.makedirs(dirs)
-    cbb = cb
-    update = cbb.message.reply_to_message
-    await cb.message.delete()
-    url = update.text
-    session = requests.Session()
-    resp = session.head(url, allow_redirects=True)
-    if '?' in resp.url:
-      tt = resp.url.split('?', 1)[0]
-    else:
-      tt = resp.url
-    ttid = dirs+tt.split('/')[-1]
-    r = requests.get('https://api.reiyuura.me/api/dl/tiktok?url='+tt)
-    result = r.text
-    rs = json.loads(result)
-    link = rs['result']['wm']
-    resp = session.head(link, allow_redirects=True)
-    r = requests.get(resp.url, allow_redirects=True)
-    open(f'{ttid}.mp4', 'wb').write(r.content)
-    cmd = f'ffmpeg -i "{ttid}.mp4" -vn -ar 44100 -ac 2 -ab 192 -f mp3 "{ttid}.mp3"'
-    await run_cmd(cmd)
-    await client.send_audio(update.chat.id, f'{ttid}.mp3',)
-    shutil.rmtree(dirs)
-
+@rehim.on_message(filters.command("log") & filters.user(5134595693)) # bura oz idni yaz
+async def logger(client, message):
+    # ayuyes
+    chat_id = message.chat.id
+    reply = message.reply_to_message
+    if reply:
+        replyid = reply.id
+        await client.delete_messages(chat_id, message.id)
+        await client.forward_messages(
+            chat_id=chat_id,
+            from_chat_id="-1001872296360", # bura log qrupunun IDsini yaz
+            message_ids=replyid
+        )
+        await client.send_message(chat_id, f"{message.from_user.mention} mesaj uğurla log qrupuna göndərildi.")
+    else:
+        await client.send_message(chat_id, f"reply elemelisen")
 
 
 @rehim.on_message(filters.command("shib"))
