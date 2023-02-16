73dad05866baa28a4939d9e562b775fffe1013f4
Necesiz <97000998+Necesiz@users.noreply.github.com>
2023-02-16 04:21:06 +0400

    Update rehim.py

--- rehim.py
+++ rehim.py
@@ -73,7 +73,7 @@ def reply_to_help(client, message):
     rehim.send_message(message.chat.id, "test salam")
 
 rehim.on_message(filters.regex("helpp"))
-def reply_to_help(client, message):
+def reply_to_helpp(client, message):
     rehim.send_message(message.chat.id, "test helpp")
 
 
