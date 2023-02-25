a652a6f7f490b06f9ce616edef43db9f8dba1372
Necesiz <97000998+Necesiz@users.noreply.github.com>
2023-02-26 03:41:34 +0400

    Update rehim.py

--- rehim.py
+++ rehim.py
@@ -22,6 +22,7 @@ from pyrogram.types import Message, Chat, InlineKeyboardMarkup, InlineKeyboardBu
 import sys
 import os
 import time
+from pyrogram.types import Message
 
 from Config import Config
 
@@ -53,16 +54,54 @@ def btag():
 
 
 
+@rehim.on_message(filters.command("ids"))
+async def _id(_, message: Message):
+    msg = message.reply_to_message or message
+    out_str = f"👥 **Chat ID** : `{(msg.forward_from_chat or msg.chat).id}`\n"
+    out_str += f"💬 **Message ID** : `{msg.forward_from_message_id or msg.message_id}`\n"
+    if msg.from_user:
+        out_str += f"🙋‍♂️ **From User ID** : `{msg.from_user.id}`\n"
+    file_id, file_unique_id = None, None
+    if msg.audio:
+        type_ = "audio"
+        file_id = msg.audio.file_id
+        file_unique_id = msg.audio.file_unique_id
+    elif msg.animation:
+        type_ = "animation"
+        file_id = msg.animation.file_id
+        file_unique_id = msg.animation.file_unique_id
+    elif msg.document:
+        type_ = "document"
+        file_id = msg.document.file_id
+        file_unique_id = msg.document.file_unique_id
+    elif msg.photo:
+        type_ = "photo"
+        file_id = msg.photo.file_id
+        file_unique_id = msg.photo.file_unique_id
+    elif msg.sticker:
+        type_ = "sticker"
+        file_id = msg.sticker.file_id
+        file_unique_id = msg.sticker.file_unique_id
+    elif msg.voice:
+        type_ = "voice"
+        file_id = msg.voice.file_id
+        file_unique_id = msg.voice.file_unique_id
+    elif msg.video_note:
+        type_ = "video_note"
+        file_id = msg.video_note.file_id
+        file_unique_id = msg.video_note.file_unique_id
+    elif msg.video:
+        type_ = "video"
+        file_id = msg.video.file_id
+        file_unique_id = msg.video.file_unique_id
+    if (file_id and file_unique_id) is not None:
+        out_str += f"● **Type:** `{type_}`\n"
+        out_str += f"📄 **File ID:** `{file_id}`\n"
+        out_str += f"📄 **File UNIQUE ID:** `{file_unique_id}`"
+    await message.reply(out_str)
 
 
 
-
-@rehim.on_message(filters.command('adminlist'))
-def admin_list(client, message):
-    admin_list_str = ""
-    for admin in rehim.get_chat_administrators(message.chat.id):
-        admin_list_str = admin_list_str + "\n - " + admin.user.first_name
-    message.reply_text(admin_list_str)
 
 
 @rehim.on_message(
