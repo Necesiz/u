8bb6a5d2b1e1cac98bd4fa5d640a60f876ca118e
Necesiz <97000998+Necesiz@users.noreply.github.com>
2023-02-26 14:13:32 +0400

    Update rehim.py

--- rehim.py
+++ rehim.py
@@ -459,24 +459,6 @@ def chat_members(client, message):
 
 
 
-# Target chat. Can also be a list of multiple chat ids/usernames
-TARGET = -1001724090128
-# Welcome message template
-MESSAGE = "{} XOW GELDIN  [Pyrogram](https://t.me/rehimbottest)'SOHBET QROUPUNA {}!"
-
-
-
-
-# Filter in only new_chat_members updates generated in TARGET chat
-@rehim.on_message(filters.chat(TARGET) & filters.new_chat_members)
-async def welcome(client, message):
-    # Build the new members list (with mentions) by using their first_name
-    new_members = [u.mention for u in message.new_chat_members]
-    # Build the welcome message by using an emoji and the list we built above
-    text = MESSAGE.format(emoji.SPARKLES, ", ".join(new_members))
-    # Send the welcome message, without the web page preview
-    await message.reply_text(text, disable_web_page_preview=True)
-
 
 
 
